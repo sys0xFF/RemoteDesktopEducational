1c458ce6fb1cc442a58208a5837a100804ca4358
Anthony Sforzin <108911718+sys0xFF@users.noreply.github.com>
2025-03-17 04:11:55 -0300

    Add files via upload

---
+++ RemoteDesktopClient.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace server
+{
+
+    public partial class RemoteDesktopClient: Form
+    {
+
+        private PictureBox pictureBox;
+        private TcpClient client;
+        private NetworkStream ns;
+        private Thread receiveThread;
+
+        public RemoteDesktopClient()
+        {
+            InitializeComponent();
+            RemoteDesktop();
+        }
+
+        void RemoteDesktop()
+        {
+
+            this.Text = "Remote Desktop";
+            this.ClientSize = new Size(800, 600);
+
+            pictureBox = new PictureBox
+            {
+                Dock = DockStyle.Fill,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            this.Controls.Add(pictureBox);
+
+ 
+            ConnectToServer();
+        }
+        private void ConnectToServer()
+        {
+            try
+            {
+                
+                client = new TcpClient("127.0.0.1", 9999);
+                ns = client.GetStream();
+
+                receiveThread = new Thread(ReceiveImages)
+                {
+                    IsBackground = true
+                };
+                receiveThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void ReceiveImages()
+        {
+            try
+            {
+                while (true)
+                {
+                  
+                    byte[] sizeBytes = new byte[4];
+                    int bytesRead = ns.Read(sizeBytes, 0, 4);
+                    if (bytesRead < 4)
+                        break;
+                    if (BitConverter.IsLittleEndian)
+                        Array.Reverse(sizeBytes);
+                    int imageSize = BitConverter.ToInt32(sizeBytes, 0);
+
+                 
+                    byte[] imgBytes = new byte[imageSize];
+                    int totalRead = 0;
+                    while (totalRead < imageSize)
+                    {
+                        int read = ns.Read(imgBytes, totalRead, imageSize - totalRead);
+                        if (read == 0)
+                            break;
+                        totalRead += read;
+                    }
+
+                    MemoryStream ms = new MemoryStream(imgBytes);
+                    Image img = Image.FromStream(ms);
+                    ms.Dispose();
+
+                    this.Invoke((MethodInvoker)delegate {
+                        pictureBox.Image = img;
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                ns?.Close();
+                client?.Close();
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (receiveThread != null && receiveThread.IsAlive)
+                receiveThread.Abort();
+            ns?.Close();
+            client?.Close();
+        }
+
+    }
+}
---
+++ RemoteDesktopServer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace RemoteDesktopEducational
+{
+    class RemoteDesktopServer
+    {
+        static void Main()
+        {
+            int port = 9999;
+            TcpListener server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+            Console.WriteLine("Waiting for client connection...");
+
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Connected client: " + client.Client.RemoteEndPoint);
+
+            NetworkStream ns = client.GetStream();
+
+            
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+           
+            int delay = 20; 
+
+            
+            while (true)
+            {
+              
+                Bitmap bmp = new Bitmap(screenWidth, screenHeight);
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(0, 0, 0, 0, new Size(screenWidth, screenHeight));
+                }
+
+              
+                MemoryStream ms = new MemoryStream();
+                ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+                EncoderParameters encoderParams = new EncoderParameters(1);
+                encoderParams.Param[0] = new EncoderParameter(Encoder.Quality, 50L);
+                bmp.Save(ms, jpgEncoder, encoderParams);
+
+                byte[] imgBytes = ms.ToArray();
+
+            
+                byte[] sizeBytes = BitConverter.GetBytes(imgBytes.Length);
+                if (BitConverter.IsLittleEndian)
+                    Array.Reverse(sizeBytes);
+                ns.Write(sizeBytes, 0, sizeBytes.Length);
+
+            
+                ns.Write(imgBytes, 0, imgBytes.Length);
+
+            
+                ms.Dispose();
+                bmp.Dispose();
+
+              
+                Thread.Sleep(delay);
+            }
+        }
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                    return codec;
+            }
+            return null;
+        }
+    }
+}
